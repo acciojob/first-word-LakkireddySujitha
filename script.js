ecdc03cef1651a5159853ace60568e5ea44de932
LakkireddySujitha <lakkireddy.sujitha1234@gmail.com>
2024-07-02 06:51:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	  s = s.trim();
+  
+  // Find the index of the first space
+  let spaceIndex = s.indexOf(' ');
+  
+  // If there is no space, return the entire string
+  if (spaceIndex === -1) {
+    return s;
+  }
+  
+  // Return the substring from the beginning up to the first space
+  return s.substring(0, spaceIndex);
+
 }
 
 // Do not change the code below
